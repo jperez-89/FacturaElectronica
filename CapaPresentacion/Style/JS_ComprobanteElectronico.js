e4a214bd5c31ba49e6efa6077698d663e83ac526
Jairo Pérez Rodríguez <jlperezro@est.utn.ac.cr>
2020-05-26 13:45:27 -0600

    G

--- CapaPresentacion/Style/JS_ComprobanteElectronico.js
+++ CapaPresentacion/Style/JS_ComprobanteElectronico.js
@@ -510,7 +510,7 @@ $(document).on('click', '#BtnFacturar', function (e) {
 
         var EncaVenta = JSON.stringify({
             Action: 'I',
-            UserID = $("#UserId").text,
+            UserID = $("#UserId").val(),
             //NumFact = NFactura + 1,
             FechaFact: $("#TxtFecha").val(),
             ClientDNI: $('#TxtIdentificacion').val(),
